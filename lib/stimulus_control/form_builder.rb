d7672ca00838f3c0b68dc3c9309cb064e7e77885
Bernardo Gurgel <bernardogfilho@gmail.com>
2020-07-11 12:16:19 +0200

    refactor: extract default class names from FormBuilder

--- lib/stimulus_control/form_builder.rb
+++ lib/stimulus_control/form_builder.rb
@@ -1,5 +1,11 @@
 module StimulusControl
   class FormBuilder < ActionView::Helpers::FormBuilder
+
+    @@label_class = "label"
+    @@field_class = "field"
+    @@error_class = "error"
+
+
     def text_field(object_name, method, options = {}, &block)
       field_tag(:text_field_tag, object_name, method, options, &block)
     end
@@ -15,7 +21,7 @@ module StimulusControl
     private
   
     def label_tag(label)
-      @template.label_tag(label, nil, class: "block mb-2 font-semibold text-sm", data: { target: "field.label" })
+      @template.label_tag(label, nil, class: @@label_class, data: { target: "field.label" })
     end
   
     def field_tag(method_name, object_name, method, options, &block)
@@ -33,18 +39,17 @@ module StimulusControl
             pattern: options[:pattern],
             required: options[:required]
           },
-          class: "px-2 py-1 text-sm block outline-none focus:outline-none focus:shadow-outline border border-gray-600",
+          class: @@field_class,
           &block)+@template.content_tag(
             "div", 
             nil, 
             data: {
               target: 'field.error'
             },
-            class: 'text-sm text-red-600 mb-4'
+            class: @@error_class
           ),
         data: {
           controller: 'field',
-          i18n: I18n.t("sform").to_json.camelize(:lower)
         }
       )
     end
